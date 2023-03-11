0fc5fdf4777c5c8e078d1a59da7a4c720eeac93e
Matt Martin <matt.martin@sine.co>
2023-03-11 17:51:34 +1030

    deadly and swamp dash added, fixed menu on mobile

--- src/components/Navigation/Toolbar.tsx
+++ src/components/Navigation/Toolbar.tsx
@@ -1,6 +1,9 @@
 import { Toolbar as MuiToolbar, IconButton, Typography } from "@mui/material";
 import { Menu } from "@mui/icons-material";
+import { Theme } from "@mui/material/styles";
+
 // import Image from "mui-image";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 interface ToolbarProps {
   open: boolean;
@@ -8,20 +11,26 @@ interface ToolbarProps {
 }
 
 export default function Toolbar({ open, handleDrawerOpen }: ToolbarProps) {
+  const isMobile = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.down("md")
+  );
+
   return (
     <MuiToolbar color="transparent">
-      <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        onClick={handleDrawerOpen}
-        edge="start"
-        sx={{
-          marginRight: 5,
-          ...(open && { display: "none" }),
-        }}
-      >
-        <Menu />
-      </IconButton>
+      {!isMobile && (
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          sx={{
+            marginRight: 5,
+            ...(open && { display: "none" }),
+          }}
+        >
+          <Menu />
+        </IconButton>
+      )}
       {/* <Image src="/AppIcon.png" width={50} /> */}
       <Typography variant="h6" noWrap component="div" sx={{ color: "black" }}>
         {process.env.REACT_APP_TITLE}
--- src/data.js
+++ src/data.js
@@ -17,6 +17,22 @@ const data = {
       scratchId: "780567654",
       description: "Ping pong with an egg.",
     },
+    {
+      id: "deadly-dash",
+      img: "./DeadlyDash.png",
+      title: "Deadly Dash",
+      author: "Vincent",
+      scratchId: "817534150",
+      description: "A ninja rabbit goes on a spooky adventure",
+    },
+    {
+      id: "swamp-dash",
+      img: "./SwampDash.png",
+      title: "Swamp Dash",
+      author: "Matt",
+      scratchId: "806110985",
+      description: "A slimy frog flees for survival.",
+    },
   ],
 };
 
