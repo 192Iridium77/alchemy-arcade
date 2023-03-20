87a3ce96e0f4d76f63ff1c396d7514a62003e0aa
Matt Martin <matt.martin@sine.co>
2023-03-21 10:04:58 +1030

    attempt

--- src/pages/Snes/Game.jsx
+++ src/pages/Snes/Game.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Box } from "@mui/material";
+import { useCallback } from "react";
+import streamToBlob from "stream-to-blob";
 // import data from "./data";
 // import { useParams } from "react-router-dom";
 // import useMediaQuery from "@mui/material/useMediaQuery";
@@ -9,9 +11,72 @@ let snes9x;
 export default function Game() {
   const [fileCount, setFileCount] = useState(0);
 
+  const initFromData = useCallback(
+    (data, name) => {
+      console.log("🚀 ~ file: snes9xNext.js:82 ~ initFromData ~ name:", name);
+      console.log("🚀 ~ file: snes9xNext.js:82 ~ initFromData ~ data:", data);
+      var dataView = new Uint8Array(data);
+      snes9x.Module.FS_createDataFile("/", name, dataView, true, false);
+      setFileCount((count) => count - 1);
+      if (fileCount === 0) {
+        snes9x.Module.FS_createFolder("/", "etc", true, true);
+        var config = "input_player1_select = shift\n";
+        var latency = parseInt(document.getElementById("latency").value, 10);
+        if (isNaN(latency)) latency = 96;
+        config += "audio_latency = " + latency + "\n";
+        if (document.getElementById("vsync").checked)
+          config += "video_vsync = true\n";
+        else config += "video_vsync = false\n";
+        snes9x.Module.FS_createDataFile(
+          "/etc",
+          "retroarch.cfg",
+          config,
+          true,
+          true
+        );
+        document.getElementById("canvas_div").style.display = "block";
+        document.getElementById("vsync").disabled = true;
+        document.getElementById("vsync-label").style.color = "gray";
+        document.getElementById("latency").disabled = true;
+        document.getElementById("latency-label").style.color = "gray";
+        console.log("Starting up");
+        snes9x.Module["callMain"](snes9x.Module["arguments"]);
+      }
+    },
+    [fileCount]
+  );
+
+  const runEmulator = useCallback(
+    (file) => {
+      // what happens if you initFromData more than once??
+      const filereader = new FileReader();
+      filereader.file_name = file.name;
+      filereader.onload = function () {
+        initFromData(this.result, this.file_name);
+      };
+      filereader.readAsArrayBuffer(file);
+    },
+    [initFromData]
+  );
+
   useEffect(() => {
     snes9x = require("./snes9xNext");
-  }, []);
+
+    (async () => {
+      const response = await fetch("./TheLegendOfZeldaALinkToThePast.smc");
+      if (!response.ok) {
+        throw new Error(
+          `Failed to fetch file: ${response.status} ${response.statusText}`
+        );
+      }
+      const blob = await response.blob();
+
+      console.log("🚀 ~ file: Game.jsx:69 ~ .then ~ blob:", blob);
+      const file = new File([blob], "The Legend of Zelda - A Link to the Past");
+      console.log("🚀 ~ file: Game.jsx:76 ~ .then ~ file:", file);
+      runEmulator(file);
+    })();
+  }, [runEmulator]);
   //   const { gameId } = useParams();
 
   //   const isMobile = useMediaQuery((theme) => theme.breakpoints.down("md"));
@@ -20,57 +85,7 @@ export default function Game() {
   //     setGame(data.find((game) => game.id === gameId));
   //   }, [gameId]);
 
-  const runEmulator = (files) => {
-    console.log("🚀 ~ file: snes9xNext.js:67 ~ runEmulator ~ files:", files);
-    setFileCount(files.length);
-    document.getElementById("openrom").innerHTML = "";
-    document.getElementById("openrom").style.display = "none";
-    for (var i = 0; i < files.length; i++) {
-      // what happens if you initFromData more than once??
-      const filereader = new FileReader();
-      filereader.file_name = files[i].name;
-      filereader.onload = function () {
-        initFromData(this.result, this.file_name);
-      };
-      filereader.readAsArrayBuffer(files[i]);
-    }
-  };
-
-  const initFromData = (data, name) => {
-    console.log("🚀 ~ file: snes9xNext.js:82 ~ initFromData ~ name:", name);
-    console.log("🚀 ~ file: snes9xNext.js:82 ~ initFromData ~ data:", data);
-    var dataView = new Uint8Array(data);
-    snes9x.Module.FS_createDataFile("/", name, dataView, true, false);
-    setFileCount((count) => count - 1);
-    if (fileCount === 0) {
-      snes9x.Module.FS_createFolder("/", "etc", true, true);
-      var config = "input_player1_select = shift\n";
-      var latency = parseInt(document.getElementById("latency").value, 10);
-      if (isNaN(latency)) latency = 96;
-      config += "audio_latency = " + latency + "\n";
-      if (document.getElementById("vsync").checked)
-        config += "video_vsync = true\n";
-      else config += "video_vsync = false\n";
-      snes9x.Module.FS_createDataFile(
-        "/etc",
-        "retroarch.cfg",
-        config,
-        true,
-        true
-      );
-      document.getElementById("canvas_div").style.display = "block";
-      document.getElementById("vsync").disabled = true;
-      document.getElementById("vsync-label").style.color = "gray";
-      document.getElementById("latency").disabled = true;
-      document.getElementById("latency-label").style.color = "gray";
-      console.log("Starting up");
-      snes9x.Module["callMain"](snes9x.Module["arguments"]);
-    }
-  };
-
-  const handleFile = (event) => {
-    runEmulator(event.target.files);
-  };
+  const handleFile = () => {};
 
   return (
     <Box
