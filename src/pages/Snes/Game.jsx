9f0e504f6bbc7c731667d2490dfa91be07cf48f7
Matt Martin <matt.martin0108@gmail.com>
2023-03-25 13:35:15 +1030

    buttons and conrols, module moved to react, progress bar not quite working?

--- src/pages/Snes/Game.jsx
+++ src/pages/Snes/Game.jsx
@@ -1,45 +1,104 @@
 import React, { useEffect, useState } from "react";
-import { Box } from "@mui/material";
 import { useCallback } from "react";
+import { Icon } from "alchemy-tech-ui";
 // import data from "./data";
 // import { useParams } from "react-router-dom";
 import { Helmet } from "react-helmet";
 
+import styled from "styled-components";
+
+const GameContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-grow: 1;
+  height: 100%;
+  width: 100%;
+`;
+
+const CanvasContainer = styled.div`
+  position: relative;
+`;
+
+const StyledCanvas = styled.canvas`
+  background-color: black;
+  display: block;
+`;
+
+const CanvasControls = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  opacity: 1;
+  padding-top: 30px;
+`;
+//   transition: opacity 0.5s ease-in-out;
+//   :hover {
+//     opacity: 1;
+//   }
+
+const EmulatorOutput = styled.div`
+  background-color: black;
+  color: lime;
+  padding: 8px;
+  overflow: auto;
+  width: 100%;
+  position: absolute;
+  height: 100px;
+  bottom: 0;
+`;
+
+const LoadingDisplay = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 16px;
+  gap: 8px;
+  opacity: 0.8;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: end;
+  padding: 16px;
+  gap: 8px;
+  opacity: 0.8;
+`;
+
 export default function Game() {
   const [pollTimer, setPollTimer] = useState();
-  const [loading, setLoading] = useState(true);
-  const [vsync, setVsync] = useState(false);
-  const [audioLatency, setAudioLatency] = useState(96);
+  const [loading, setLoading] = useState("loading"); //typeme
+  const [progress, setProgress] = useState(0); //typeme
+  const [emulatorOutput, setEmulatorOutput] = useState("");
+  const [showOutput, setShowOutput] = useState(false);
 
-  const initFromData = useCallback(
-    (data, name) => {
-      const dataView = new Uint8Array(data);
-      window.Module.FS_createDataFile("/", name, dataView, true, true);
-      window.Module.FS_createFolder("/", "etc", true, true);
-      let config = "input_player1_select = shift\n";
-      let latency = parseInt(audioLatency, 10);
-      if (isNaN(latency)) latency = 96;
-      config += "audio_latency = " + latency + "\n";
-      if (vsync) config += "video_vsync = true\n";
-      else config += "video_vsync = false\n";
-      window.Module.FS_createDataFile(
-        "/etc",
-        "retroarch.cfg",
-        config,
-        true,
-        true
-      );
-      window.Module["callMain"](window.Module["arguments"]);
+  const initFromData = useCallback((data, name) => {
+    const dataView = new Uint8Array(data);
+    window.Module.FS_createDataFile("/", name, dataView, true, true);
+    window.Module.FS_createFolder("/", "etc", true, true);
+    let config = "input_player1_select = shift\n";
+    config += "audio_latency = 96\n";
+    config += "video_vsync = true\n";
+    window.Module.FS_createDataFile(
+      "/etc",
+      "retroarch.cfg",
+      config,
+      true,
+      true
+    );
+    window.Module["callMain"](window.Module["arguments"]);
 
-      setLoading(false);
-    },
-    [vsync, audioLatency]
-  );
+    setLoading("");
+  }, []);
 
   const runEmulator = useCallback(
     (gameData) => {
+      // todo track internal initialisation and remove polling
       const timer = setInterval(() => {
-        if (window.Module) {
+        if (window.Module?.FS_createDataFile) {
           initFromData(gameData, "TheLegendOfZelda.smc");
         }
       }, 50);
@@ -50,7 +109,56 @@ export default function Game() {
   );
 
   useEffect(() => {
-    setLoading(true);
+    setLoading("initialising");
+
+    window.Module = {
+      noInitialRun: true, // sets shouldRunNow to false which will initialise, but not start the emulator. emulator can then probably start by calling Module.run() or doRun
+      arguments: ["-v", "/TheLegendOfZelda.smc"], // libretro cli args, add '--help' to the array for more info
+      preRun: [], // not sure how pre/post work yet, I think they are hooks that give access to internal variables at runtime
+      postRun: [],
+      print: (text) => {
+        text = Array.prototype.slice.call(arguments).join(" "); // arguments is global??
+        setEmulatorOutput((value) => {
+          return `${value}${text}\n`;
+        });
+      },
+      printErr: function (text) {
+        // todo toast
+        text = Array.prototype.slice.call(arguments).join(" ");
+        setEmulatorOutput((value) => {
+          return `${value}${text}\n`;
+        });
+      },
+      canvas: document.getElementById("canvas"),
+      setProgress: function (remaining, expected) {
+        console.log(
+          "🚀 ~ file: Game.jsx:110 ~ useEffect ~ remaining:",
+          remaining
+        );
+
+        setProgress((expected - remaining) / expected);
+      },
+      setStatus: function (text) {
+        console.log("🚀 ~ file: Game.jsx:108 ~ useEffect ~ text:", text);
+        setLoading(text);
+        if (window.Module.setStatus.interval)
+          clearInterval(this.setStatus.interval); // why is this here????
+      },
+      totalDependencies: 0,
+      monitorRunDependencies: function (remaining) {
+        this.totalDependencies = Math.max(this.totalDependencies, remaining);
+
+        if (remaining) {
+          setProgress(
+            this.totalDependencies - remaining / this.totalDependencies
+          );
+        } else {
+          setLoading("complete");
+        }
+      },
+    };
+    window.Module.setStatus("downloading");
+
     (async () => {
       const response = await fetch("/TheLegendOfZeldaALinkToThePast.smc", {
         responseType: "arraybuffer",
@@ -80,98 +188,47 @@ export default function Game() {
     if (!loading) {
       clearInterval(pollTimer);
     }
-  });
-
-  const handleChangeVsync = () => {
-    setVsync(!vsync);
-  };
-  const handleAudioLatency = (event) => {
-    setAudioLatency(event.target.value);
-  };
+  }, [loading, pollTimer]);
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        flexGrow: 1,
-        height: "100%",
-        width: "100%",
-        // p: isMobile ? 0 : 4,
-      }}
-    >
+    <GameContainer>
       <div style={{ marginTop: "12px" }}>
-        <div className="emscripten" id="status"></div>
-        <div>
-          <progress defaultValue="0" max="1" id="progress" hidden=""></progress>
-        </div>
-        <div id="canvas_div">
-          <canvas
+        <CanvasContainer>
+          <StyledCanvas
             className="emscripten"
             id="canvas"
             onContextMenu={(event) => event.preventDefault()}
             width="800"
             height="600"
-            style={{ backgroundColor: "black" }}
-          ></canvas>
-        </div>
-
-        <div className="emscripten">
-          <input type="checkbox" id="resize" />
-          <label htmlFor="resize">Resize canvas</label>
-          <input type="checkbox" id="pointerLock" defaultChecked />
-          <label htmlFor="pointerLock">Lock/hide mouse pointer</label>
-          <input
-            type="button"
-            value="Fullscreen"
-            onClick={() =>
-              window.Module.requestFullScreen(
-                document.getElementById("pointerLock").checked,
-                document.getElementById("resize").checked
-              )
-            }
           />
-          <br />
-          <input
-            id="vsync"
-            type="checkbox"
-            checked={vsync}
-            disabled={!loading}
-            onChange={handleChangeVsync}
-          />
-          <label htmlFor="vsync" style={{ color: !loading && "gray" }}>
-            Enable V-sync (can only be done before loading game)
-          </label>
-          <br />
-          <input
-            type="textbox"
-            id="latency"
-            size="3"
-            maxLength="3"
-            value={audioLatency}
-            onChange={handleAudioLatency}
-            disabled={!loading}
-          />
-          <label
-            htmlFor="latency"
-            id="latency-label"
-            style={{ color: !loading && "gray" }}
-          >
-            Audio latency (ms) (increase if you hear pops at fullspeed, can only
-            be done before loading game)
-          </label>
-        </div>
-        <textarea
-          className="emscripten"
-          id="output"
-          rows="8"
-          style={{ width: "100%" }}
-        ></textarea>
+          {showOutput ? (
+            <EmulatorOutput>
+              <div style={{ padding: "4px" }}>{emulatorOutput}</div>
+            </EmulatorOutput>
+          ) : null}
+          <LoadingDisplay>
+            <div style={{ color: "white" }}>{loading}</div>
+            <progress max={1} value={progress} hidden={!loading} />
+          </LoadingDisplay>
+          <CanvasControls>
+            <ButtonGroup>
+              <Icon
+                type="CommandLine"
+                onClick={() => setShowOutput(!showOutput)}
+                color="white"
+              />
+              <Icon
+                type="Expand"
+                onClick={() => window.Module.requestFullScreen(true, true)}
+                color="white"
+              />
+            </ButtonGroup>
+          </CanvasControls>
+        </CanvasContainer>
       </div>
       <Helmet>
         <script src="/snes9x.js" type="text/javascript" />
       </Helmet>
-    </Box>
+    </GameContainer>
   );
 }
